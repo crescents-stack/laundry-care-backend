2793c87df8ea28e2fa39bd7e57032edea7287806
musiur <musiur.opu@gmail.com>
2023-10-13 15:47:51 +0600

    [Enchancement] Payment Gateway Stripe #19

--- controllers/stripe.controller.js
+++ controllers/stripe.controller.js
@@ -3,7 +3,6 @@ const MakePaymentIntendWithAmount = require("../config/stripe");
 
 exports.GetSecret = async (req, res) => {
   try {
-    console.log(req.body.amount, "Amount")
     const intent = await MakePaymentIntendWithAmount(req.body.amount || 0);
     if (!intent) {
       res.status(500).send({
